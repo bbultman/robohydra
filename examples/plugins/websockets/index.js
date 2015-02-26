d30f1fb02cf08e87c6cf1b137b93c48ab084b98f
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2015-02-26 21:18:50 +0100

    WebSocket support fixes

--- examples/plugins/websockets/index.js
+++ examples/plugins/websockets/index.js
@@ -1,14 +1,49 @@
-var heads                  = require("robohydra").heads,
-    RoboHydraWebSocketHead = heads.RoboHydraWebSocketHead;
+var heads                   = require("robohydra").heads,
+    RoboHydraHead           = heads.RoboHydraHead,
+    RoboHydraWebSocketHead  = heads.RoboHydraWebSocketHead,
+    RoboHydraHeadFilesystem = heads.RoboHydraHeadFilesystem;
 
 module.exports.getBodyParts = function() {
+    var socket;
+
     return {
+        heads: [
+            new RoboHydraHead({
+                name: 'message-sender',
+                path: '/send-message',
+                handler: function(req, res) {
+                    var msg = req.queryParams.message || 'Default message';
+                    if (socket) {
+                        socket.send(msg);
+                        res.send('Sent message');
+                    } else {
+                        res.send('Do not have any websocket yet');
+                    }
+                }
+            }),
+
+            new RoboHydraHeadFilesystem({
+                name: 'static-files',
+                mountPath: '/',
+                documentRoot: 'examples/websockets'
+            })
+        ],
+
         webSocketHeads: [
             new RoboHydraWebSocketHead({
                 path: '/.*',
-                handler: function(req, socket) {
+                handler: function(req, sock) {
                     console.log("Hey, I received a connection from URL path " + req.url);
-                    console.log("And I have a socket: " + socket);
+
+                    sock.send('test data');
+                    sock.on('message', function(msg) {
+                        console.log("Received mesage " + msg);
+                    });
+                    sock.on('error', function() {
+                        console.log("Closed connection, boohoo!");
+                    });
+
+                    socket = sock;
                 }
             })
         ]
